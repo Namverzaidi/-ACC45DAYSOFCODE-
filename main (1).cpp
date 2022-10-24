b177ad96d02af52647e204404dcd70b2eee7f7b0
Namverzaidi <116440216+Namverzaidi@users.noreply.github.com>
2022-10-24 15:18:34 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+class Employee {
+  private:
+    
+    int salary;
+
+  public:
+    void setSalary(int s) {
+      salary = s;
+    }
+    int getSalary() {
+      return salary;
+    }
+};
+
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  cout << myObj.getSalary();
+  return 0;
+}
