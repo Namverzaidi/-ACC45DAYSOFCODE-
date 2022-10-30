679433717e086e865f2ba2b62cf1d9d3ecfa846f
Namverzaidi <116440216+Namverzaidi@users.noreply.github.com>
2022-10-30 22:08:42 +0530

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double number, squareRoot;
+    
+    number = 25.0;
+
+
+    squareRoot = sqrt(number);
+
+    cout << "Square root of " << number << " = " << squareRoot;
+
+    return 0;
+}
