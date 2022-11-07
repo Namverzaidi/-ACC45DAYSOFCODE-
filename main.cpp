4e04b7371e1912d77f03de2f1465dac6ec532e13
Namverzaidi <116440216+Namverzaidi@users.noreply.github.com>
2022-11-07 23:36:55 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-#include <iostream>
+//write a program in C++ to print Floyd's Triangle
 
+
+#include<iostream>
 using namespace std;
-
-class Car {        // The class
-  public:          // Access specifier
-    string brand;  // Attribute
-    string model;  // Attribute
-    int year;      // Attribute
-    Car(string x, string y, int z) { // Constructor with parameters
-      brand = x;
-      model = y;
-      year = z;
+int main()
+{
+    int i, j, num=1;
+    for(i=0; i<5; i++)
+    {
+        for(j=0; j<=i; j++)
+        {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
     }
-};
-
-int main() {
-  // Create Car objects and call the constructor with different values
-  Car carObj1("BMW", "X5", 1999);
-  Car carObj2("Ford", "Mustang", 1969);
-
-  // Print values
-  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
-  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
-  return 0;
+    return 0;
 }
