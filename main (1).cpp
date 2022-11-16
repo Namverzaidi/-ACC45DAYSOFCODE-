63430970f2ec9aabced51439a436db8404b5145c
Namverzaidi <116440216+Namverzaidi@users.noreply.github.com>
2022-11-16 16:30:27 +0530

    Add files via upload

--- main (1).cpp
+++ main (1).cpp
@@ -1,19 +1,52 @@
-//write a program in C++ to check whether a year is a leap year or not. 
+// Add Two Matrices using Multi-dimensional Arrays
 
 
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    int yr;
-    cout<<"Enter the Year: ";
-    cin>>yr;
-    if((yr%4==0) && (yr%100!=0))
-        cout<<"\nIt is a Leap Year";
-    else if(yr%400==0)
-        cout<<"\nIt is a Leap Year";
-    else
-        cout<<"\nIt is not a Leap Year";
-    cout<<endl;
+    int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+
+    cout << "Enter number of rows (between 1 and 100): ";
+    cin >> r;
+
+    cout << "Enter number of columns (between 1 and 100): ";
+    cin >> c;
+
+    cout << endl << "Enter elements of 1st matrix: " << endl;
+
+    // Storing elements of first matrix entered by user.
+    for(i = 0; i < r; ++i)
+       for(j = 0; j < c; ++j)
+       {
+           cout << "Enter element a" << i + 1 << j + 1 << " : ";
+           cin >> a[i][j];
+       }
+
+    // Storing elements of second matrix entered by user.
+    cout << endl << "Enter elements of 2nd matrix: " << endl;
+    for(i = 0; i < r; ++i)
+       for(j = 0; j < c; ++j)
+       {
+           cout << "Enter element b" << i + 1 << j + 1 << " : ";
+           cin >> b[i][j];
+       }
+
+    // Adding Two matrices
+    for(i = 0; i < r; ++i)
+        for(j = 0; j < c; ++j)
+            sum[i][j] = a[i][j] + b[i][j];
+
+    // Displaying the resultant sum matrix.
+    cout << endl << "Sum of two matrix is: " << endl;
+    for(i = 0; i < r; ++i)
+        for(j = 0; j < c; ++j)
+        {
+            cout << sum[i][j] << "  ";
+            if(j == c - 1)
+                cout << endl;
+        }
+
     return 0;
 }
