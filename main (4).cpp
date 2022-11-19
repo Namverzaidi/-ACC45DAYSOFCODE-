fef3df3759874825bc784e41ee08e0ab397f97fc
Namverzaidi <116440216+Namverzaidi@users.noreply.github.com>
2022-11-19 23:43:45 +0530

    Add files via upload

--- main (4).cpp
+++ main (4).cpp
@@ -1,22 +1,26 @@
-//write a program to print alphabet triagle
+//  Program to print half pyramid using *
 
-#include <iostream>  
-using namespace std;  
-int main()  
-{  
- char ch='A';    
-    int i, j, k, m;    
-    for(i=1;i<=5;i++)    
-    {    
-        for(j=5;j>=i;j--)    
-            cout<<" ";    
-        for(k=1;k<=i;k++)    
-            cout<<ch++;    
-            ch--;    
-        for(m=1;m<i;m++)    
-            cout<<--ch;    
-        cout<<"\n";    
-        ch='A';    
-    }    
-return 0;  
-}  
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+Example 2: Program to print half pyramid a using numbers
+1
