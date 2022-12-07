120b6191325a84e19639ae04a29abffbeb27c8d7
Namverzaidi <116440216+Namverzaidi@users.noreply.github.com>
2022-12-07 16:32:33 +0530

    Add files via upload

---
+++ main (12).cpp
@@ -0,0 +1,16 @@
+//Print ASCII Value in C++
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
+
+
